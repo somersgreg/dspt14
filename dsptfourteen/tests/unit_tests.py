bb877ec10e7ea6e85f1c3613c0157d7b1940a8ea
James Barciz <jamesjbarciz@gmail.com>
2021-11-03 21:54:34 -0500

    completed exponentiation feature

--- dsptfourteen/tests/unit_tests.py
+++ dsptfourteen/tests/unit_tests.py
@@ -13,6 +13,11 @@ def test_times_2(base_test_array):
     assert_array_equal(np.array([2, 4, 6, 8, 10]), pp.times_constant(base_test_array))
 
 
+def test_exponent_2(base_test_array):
+    pp = MyPreprocessor(constant=2)
+    assert_array_equal(np.array([1**2, 2**2, 3**2, 4**2, 5**2]), pp.times_constant(base_test_array))
+
+
 def test_plus_1_null(null_test_array):
     pp = MyPreprocessor(constant=1)
     assert_array_equal(np.array([2, 3, np.NaN, 5, 6]), pp.plus_constant(null_test_array))
